af86fa8dfa40970ada14e6cbce9601dcbe2ebf6e
linnnnk <l_ink@outlook.com>
2022-04-29 20:40:06 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package aula_um;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("meu primeiro programa");
+		System.out.println("oi oi");
+	}
+
+}
